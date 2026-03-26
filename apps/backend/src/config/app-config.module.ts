40d5f40712e5c4e99d9a0877f2b9535f5bf3c8ff
Chris Caspanello <ccaspanell@gmail.com>
2026-03-25 23:20:35 -0400

    Fixed tests

--- apps/backend/src/config/app-config.module.ts
+++ apps/backend/src/config/app-config.module.ts
@@ -2,11 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfigService } from './app-config.service';
 
+const envFilePath =
+  process.env.NODE_ENV === 'test' ? ['.env.test.local', '.env.test', '.env.local', '.env'] : ['.env.local', '.env'];
+
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env', '.env.local'],
+      envFilePath: envFilePath,
     }),
   ],
   providers: [AppConfigService],
